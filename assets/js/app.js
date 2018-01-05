0fd14d92e58bb113818ed81a7cffc83bca321b7a
daniparraguez <danielaparraguezvarela@gmail.com>
2018-01-05 05:03:01 -0300

    Actualizando hasta img review

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
 
 	$("#ingresa").click(function() {
 		var hideLogin= $("#login").hide();
-		var hideModal= $("#inicia").hide();
-		var removeFade= $("#inicia").removeClass("modal fade");
 		var showUser= $(".img-circle").show();
 
 	});
@@ -11,14 +9,31 @@ $(document).ready(function(){
 //evento al apretar botón
 	$("#post").click(function() {
 	//guardar input
-	var newReview= $("#mensajetxt").val();
-	$("#mensajetxt").val(""); //limpia area
+	var newReview= $("#review").val();
+	$("#review").val(""); //limpia area
+	var newType= $("#type").val();
+	$("#type").val(""); //limpia area
+	var newImg= $("#urlimg").val();
+	$("#urlimg").val(""); //limpia area
+	var date= new Date;
 
-//apendding inputs
-$(".contenedor").append("<div class='formcont'>"+
-	"<h4>"+mensaje+"</h4>"+"</div");
-	
 
+
+//apendding review de té input
+$(".post").append("<div class='well'>"+"<div class='media'>"+
+"<a class='pull-left' href='#'>"+
+"<img class='media-object' src="+newImg+">"+"</a>"+"<div class='media-body'>"+
+"<h4 class='media-heading'>"+newReview+"</h4>"+"<p class='text-left'>User</p>"+
+"<p class='new-review'></p>"+"<ul class='list-inline list-unstyled'>"+
+"<li><span><i class='glyphicon glyphicon-calendar'></i>" +date+"</span></li>"+
+"<li>|</li><span><i class='glyphicon glyphicon-comment'></i></span>"+
+"<li>|</li><li><span class='glyphicon glyphicon-star'></span>"+
+"<span class='glyphicon glyphicon-star'></span><span class='glyphicon glyphicon-star'>"+
+"</span><span class='glyphicon glyphicon-star'></span><span class='glyphicon glyphicon-star-empty'>"+
+"</span></li><li>|</li></ul></div></div></div></div>");
+
+//apendding tipo de té input al trending
+$("#trending").append("<a href='#'' class='list-group-item'>#"+newType+"</a>");
 
 	});
 });
